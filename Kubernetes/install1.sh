72a9cbf777e0fc915b9ca137654f214f2fc78e1f
Federico <41119122+fgelosa@users.noreply.github.com>
2024-02-12 11:40:09 +0100

    Update install1.sh

--- Kubernetes/install1.sh
+++ Kubernetes/install1.sh
@@ -34,30 +34,15 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install -y kubectl
 
+git clone https://github.com/elgris/microservice-app-example.git
 
+cd microservice-app-example
 
 minikube start
 kubectl get po -A
 
-## per la dashboard
-##minikube dashboard
+eval $(minikube docker-env)
+docker compose build
 
-
-##https://minikube.sigs.k8s.io/docs/start/
-##Seguendo il punto 4 di questa guida possiamo provare cose, ma c'e' da fare il port forwarding
-##kubectl port-forward service/hello-minikube 7080:8080 --address 0.0.0.0
-
-##Build locale delle immagini
-eval $(minikube -p minikube docker-env)
-cd microservice-app-example/auth-api
-docker build . -t thesis/auth-api
-cd ../frontend
-docker build . -t thesis/frontend
-cd ../todos-api
-docker build . -t thesis/todos-api
-cd ../users-api
-docker build . -t thesis/users-api
-cd ../..
-
-##apply dei file di conf
-kubectl apply -f k8s_config_files/
+kubectl create -R -f k8s/
+minikube service frontend
