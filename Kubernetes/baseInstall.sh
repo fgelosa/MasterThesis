ab457b9cf2007716b2708a13f4887ae031277763
Federico <41119122+fgelosa@users.noreply.github.com>
2024-02-12 10:52:03 +0100

    Update baseInstall.sh

--- Kubernetes/baseInstall.sh
+++ Kubernetes/baseInstall.sh
@@ -44,3 +44,18 @@ kubectl get po -A
 ##https://minikube.sigs.k8s.io/docs/start/
 ##Seguendo il punto 4 di questa guida possiamo provare cose, ma c'e' da fare il port forwarding
 ##kubectl port-forward service/hello-minikube 7080:8080 --address 0.0.0.0
+
+##Build locale delle immagini
+eval $(minikube -p minikube docker-env)
+cd microservice-app-example/auth-api
+docker build . -t thesis/auth-api
+cd ../frontend
+docker build . -t thesis/frontend
+cd ../todos-api
+docker build . -t thesis/todos-api
+cd ../users-api
+docker build . -t thesis/users-api
+cd ../..
+
+##apply dei file di conf
+kubectl apply -f k8s_config_files/
