66590c99a68433f23cdf8ec5c7aa78acd019206e
Federico <41119122+fgelosa@users.noreply.github.com>
2024-02-13 11:38:16 +0100

    Update install1.sh

--- Kubernetes/install1.sh
+++ Kubernetes/install1.sh
@@ -11,17 +11,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
-sudo service docker restart
-newgrp docker
-service docker status
-
-docker run hello-world
 ################################
 ### Installing minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
@@ -31,18 +21,8 @@ sudo dpkg -i minikube_latest_amd64.deb
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
 sudo apt-get update
-sudo apt-get install -y kubectl
-
+sudo apt-get install -y kubectl docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 git clone https://github.com/elgris/microservice-app-example.git
-
-cd microservice-app-example
-
-minikube start
-kubectl get po -A
-
-eval $(minikube docker-env)
-docker compose build
-
-kubectl create -R -f k8s/
-minikube service frontend
