37b21dee06a1c9d5dd5bcf6c65420e32bb9e4a9f
Federico <41119122+fgelosa@users.noreply.github.com>
2024-03-09 11:41:36 +0100

    Add files via upload

---
+++ Kubernetes/K8sConfigFiles/EnvoyImplementation/setup/setup.sh
@@ -0,0 +1,7 @@
+kubectl create configmap authz-policy --from-file frontend/policy-frontend.rego --from-file auth-api/policy.rego --from-file todos-api/policy.rego --from-file users-api/policy.rego --from-file redis-queue/policy.rego
+
+kubectl create configmap proxy-config-frontend --from-file frontend/envoy.yaml --from-file frontend/tls.crt --from-file frontend/tls.key 
+kubectl create configmap proxy-config-auth-api --from-file auth-api/envoy.yaml --from-file auth-api/tls.crt --from-file auth-api/tls.key 
+kubectl create configmap proxy-config-todos-api --from-file todos-api/envoy.yaml --from-file todos-api/tls.crt --from-file todos-api/tls.key 
+kubectl create configmap proxy-config-users-api --from-file users-api/envoy.yaml --from-file users-api/tls.crt --from-file users-api/tls.key 
+kubectl create configmap proxy-config-redis-queue --from-file redis-queue/envoy.yaml --from-file redis-queue/tls.crt --from-file redis-queue/tls.key 
---
+++ Kubernetes/K8sConfigFiles/EnvoyImplementation/setup/todos-api/policy.rego
@@ -0,0 +1,8 @@
+# policy.rego
+package envoy.authz
+
+import rego.v1
+
+import input.attributes.request.http as http_request
+
+default allow := true
