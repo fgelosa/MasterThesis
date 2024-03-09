110f2868007ce829bf3ebea2bf8c23a94e90a325
Federico <41119122+fgelosa@users.noreply.github.com>
2024-03-09 13:27:23 +0100

    Update setup.sh

--- Kubernetes/K8sConfigFiles/EnvoyImplementation/setup/setup.sh
+++ Kubernetes/K8sConfigFiles/EnvoyImplementation/setup/setup.sh
@@ -1,4 +1,5 @@
-kubectl create configmap authz-policy --from-file frontend/policy-frontend.rego --from-file auth-api/policy.rego --from-file todos-api/policy.rego --from-file users-api/policy.rego --from-file redis-queue/policy.rego
+#kubectl create configmap authz-policy --from-file frontend/policy-frontend.rego --from-file auth-api/policy-auth.rego --from-file todos-api/policy-todos.rego --from-file users-api/policy-users.rego --from-file redis-queue/policy-redis.rego --from-file policy.rego
+kubectl create configmap authz-policy --from-file policy.rego
 
 kubectl create configmap proxy-config-frontend --from-file frontend/envoy.yaml --from-file frontend/tls.crt --from-file frontend/tls.key 
 kubectl create configmap proxy-config-auth-api --from-file auth-api/envoy.yaml --from-file auth-api/tls.crt --from-file auth-api/tls.key 
